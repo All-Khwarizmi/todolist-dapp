c04cab7a649c25c0df0c15aef1c61909ab5e64bd
JASON SUAREZ <suarezjason@icloud.com>
2025-01-16 19:44:43 +0100

    feat: display user status

--- src/components/WalletConnect.tsx
+++ src/components/WalletConnect.tsx
@@ -1,11 +1,24 @@
 "use client";
 
-import React from "react";
+import React, { useState } from "react";
 import { useSyncProviders } from "../store/useSyncProvider";
 
 function WalletConnect() {
   const providers = useSyncProviders();
   const [open, setOpen] = React.useState(false);
+  const [selectedWallet, setSelectedWallet] = useState<EIP6963ProviderDetail>();
+  const [userAccount, setUserAccount] = useState<string>("");
+
+  const [errorMessage, setErrorMessage] = useState("");
+  const clearError = () => setErrorMessage("");
+  const setError = (error: string) => setErrorMessage(error);
+  const isError = !!errorMessage;
+
+  // Display a readable user address.
+  const formatAddress = (addr: string) => {
+    const upperAfterLastTwo = addr.slice(0, 2) + addr.slice(2);
+    return `${upperAfterLastTwo.substring(0, 5)}...${upperAfterLastTwo.substring(39)}`;
+  };
 
   const handleConnect = async (providerWithInfo: EIP6963ProviderDetail) => {
     try {
@@ -13,25 +26,38 @@ function WalletConnect() {
         method: "eth_requestAccounts",
       })) as string[];
 
-      console.log(accounts);
-    } catch (e) {
-      console.error(e);
+      setSelectedWallet(providerWithInfo);
+      setUserAccount(accounts?.[0]);
+      setOpen(false);
+    } catch (error) {
+      console.error(error);
+      const mmError: MMError = error as MMError;
+      setError(`Code: ${mmError.code} \nError Message: ${mmError.message}`);
     }
   };
   return (
     <div>
-      <button
-        onClick={() => {
-          setOpen(!open);
-        }}
-      >
-        Connect Wallet
-        <div
-          className={`${
-            open ? "" : "hidden"
-          } absolute z-10 top-0 left-0 w-full h-full bg-black/50 backdrop-blur-sm`}
-        ></div>
-      </button>
+      {isError && (
+        <div onClick={clearError} className="text-red-500 text-sm">
+          {errorMessage}
+        </div>
+      )}
+      {selectedWallet ? (
+        <div>{formatAddress(userAccount)}</div>
+      ) : (
+        <button
+          onClick={() => {
+            setOpen(!open);
+          }}
+        >
+          Connect Wallet
+          <div
+            className={`${
+              open ? "" : "hidden"
+            } absolute z-10 top-0 left-0 w-full h-full bg-black/50 backdrop-blur-sm`}
+          ></div>
+        </button>
+      )}
       {open && (
         <div className="absolute z-10 top-0 left-0 w-full h-full bg-white/90 backdrop-blur-sm">
           <div className="flex flex-col gap-4 p-8 w-full max-w-lg">
