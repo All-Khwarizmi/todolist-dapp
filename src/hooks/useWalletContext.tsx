f772163ec13403f25655d75e4b9ad8f1e4b5b4f6
JASON SUAREZ <suarezjason@icloud.com>
2025-01-18 20:37:10 +0100

    refactor: add error boundary in wallet context hook

--- src/hooks/useWalletContext.tsx
+++ src/hooks/useWalletContext.tsx
@@ -1,4 +1,9 @@
 import { useContext } from "react";
 import { WalletProviderContext } from "./WalletProvider";
 
-export const useWalletProvider = () => useContext(WalletProviderContext);
+export const useWalletProvider = () => {
+  if (!WalletProviderContext) {
+    throw new Error("useWalletProvider must be used within a WalletProvider");
+  }
+  useContext(WalletProviderContext);
+};
