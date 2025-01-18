2bb2776cff0ceca6b248417ee73e6cf0cd3d1ac1
JASON SUAREZ <suarezjason@icloud.com>
2025-01-18 06:48:42 +0100

    feat: add todo dto

---
+++ src/core/entities/todo.dto.ts
@@ -0,0 +1,38 @@
+import { ethers } from "ethers";
+import { Status, Todo } from "./todo";
+
+export class TodoDto {
+  static toDomain(todo: any): Todo {
+    const todoDefinition = todo[0];
+    const todoStatus = this.parseStatus(Number(todo[1]));
+    const todoCreatedAt = this.timestampToDate(Number(todo[2]));
+    const _todo: Todo = {
+      definition: todoDefinition,
+      status: todoStatus,
+      createdAt: todoCreatedAt,
+    };
+    return _todo;
+  }
+
+  static parseStatus(status: any): Status {
+    const todoStatus = ethers.formatUnits(status, "wei");
+    switch (Number(todoStatus)) {
+      case 0:
+        return Status.CREATED;
+      case 1:
+        return Status.UPDATED;
+      case 2:
+        return Status.DELETED;
+      default:
+        return Status.CREATED;
+    }
+  }
+
+  static parseTimestamp(timestamp: any): number {
+    return Number(ethers.formatUnits(timestamp, "wei"));
+  }
+
+  static timestampToDate(timestamp: any): Date {
+    return new Date(Number(ethers.formatUnits(timestamp, "wei")) * 1000);
+  }
+}
