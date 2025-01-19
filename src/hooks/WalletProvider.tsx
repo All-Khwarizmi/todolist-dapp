b62a105a6ad1dd66ee454926f44472464fbdbe0a
JASON SUAREZ <suarezjason@icloud.com>
2025-01-19 06:24:19 +0100

    refactor: declare types outside of module

--- src/hooks/WalletProvider.tsx
+++ src/hooks/WalletProvider.tsx
@@ -12,30 +12,6 @@ import { ProviderRepositoryImpl } from "../infrastructure/repositories/provider.
 import { TODO_ABI } from "../core/entities/todo-abi";
 import { TodoRepositoryImpl } from "../infrastructure/repositories/todos/todo.impl";
 
-// Type alias for a record where the keys are wallet identifiers and the values are account
-// addresses or null.
-type SelectedAccountByWallet = Record<string, string | null>;
-
-// Context interface for the EIP-6963 provider.
-interface WalletProviderContext {
-  wallets: Record<string, EIP6963ProviderDetail>; // A list of wallets.
-  selectedWallet: EIP6963ProviderDetail | null; // The selected wallet.
-  selectedAccount: string | null; // The selected account address.
-  errorMessage: string | null; // An error message.
-  ethersProvider: BrowserProvider | null;
-  todoRepository?: TodoRepository;
-  connectWallet: (walletUuid: string) => Promise<void>;
-  disconnectWallet: () => void;
-  clearError: () => void;
-  getContract: (address: string, abi: any) => Promise<Contract | null>;
-}
-
-declare global {
-  interface WindowEventMap {
-    "eip6963:announceProvider": CustomEvent;
-  }
-}
-
 export const WalletProviderContext =
   createContext<WalletProviderContext | null>(null);
 
---
+++ src/hooks/wallet-provider.d.ts
@@ -0,0 +1,23 @@
+// Type alias for a record where the keys are wallet identifiers and the values are account
+// addresses or null.
+type SelectedAccountByWallet = Record<string, string | null>;
+
+// Context interface for the EIP-6963 provider.
+interface WalletProviderContext {
+  wallets: Record<string, EIP6963ProviderDetail>; // A list of wallets.
+  selectedWallet: EIP6963ProviderDetail | null; // The selected wallet.
+  selectedAccount: string | null; // The selected account address.
+  errorMessage: string | null; // An error message.
+  ethersProvider: BrowserProvider | null;
+  todoRepository?: TodoRepository;
+  connectWallet: (walletUuid: string) => Promise<void>;
+  disconnectWallet: () => void;
+  clearError: () => void;
+  getContract: (address: string, abi: any) => Promise<Contract | null>;
+}
+
+declare global {
+  interface WindowEventMap {
+    "eip6963:announceProvider": CustomEvent;
+  }
+}
