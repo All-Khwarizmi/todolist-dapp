9a3a378a518b5321bbb0656c9d291768b35810a5
JASON SUAREZ <suarezjason@icloud.com>
2025-01-19 13:43:13 +0100

    refactor: update user balance ui style

--- src/components/users/UserBalance.tsx
+++ src/components/users/UserBalance.tsx
@@ -3,6 +3,9 @@
 import React, { useEffect } from "react";
 import { useWalletProvider } from "@/src/hooks/wallet/use-wallet-context";
 import { useGetUserBalance } from "@/src/core/usecases/users/get-balance";
+import { Badge } from "@/components/ui/badge";
+import { Skeleton } from "@/components/ui/skeleton";
+
 function UserBalance() {
   const ctx = useWalletProvider();
 
@@ -23,21 +26,15 @@ function UserBalance() {
   if (!ctx?.selectedAccount) return null;
 
   return (
-    <div className="flex  gap-4">
-      {isLoading && <p>Loading...</p>}
-      {error && <p>Error: {error.message}</p>}
-      {ctx.chainId && (
-        <p className="bg-violet-300 text-background rounded-lg p-1 px-2">
-          {" "}
-          Network: {ctx.chainId}
-        </p>
-      )}
-      {userBalance && (
-        <p className="bg-green-300 text-background rounded-lg p-1 px-2">
-          {" "}
-          Balance: {userBalance}
-        </p>
-      )}
+    <div className="flex items-center space-x-2">
+      {ctx.chainId && <Badge variant="secondary">Network: {ctx.chainId}</Badge>}
+      {isLoading ? (
+        <Skeleton className="h-5 w-[100px]" />
+      ) : error ? (
+        <Badge variant="destructive">Error: {error.message}</Badge>
+      ) : userBalance ? (
+        <Badge variant="default">Balance: {userBalance}</Badge>
+      ) : null}
     </div>
   );
 }
